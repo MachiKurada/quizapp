2900f6b2dafddfa220902a0fa502db013de66998
machikurada <machikurada@gmail.com>
2026-06-21 15:23:34 +0200

    basic card class work as intended

--- code/main.py
+++ code/main.py
@@ -1,7 +1,18 @@
 #this is the program that will run.
 
+from cards import Card
+
 def main ():
     print ("éèôà")
     print ("いろは")
+
+    card1 = Card("red", "rouge")
+    card2 = Card("goat", "chêvre")
+    card3 = Card("寿司", "sushi")
+    print(card1)
+    print (card2)
+    print(card3)
+
+    
 
 main()
