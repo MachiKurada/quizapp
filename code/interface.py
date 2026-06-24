4c5494dda713a22f454e48e1715e8905a3da8a13
machikurada <machikurada@gmail.com>
2026-06-24 15:10:56 +0200

    put quiz function in place

--- code/interface.py
+++ code/interface.py
@@ -19,20 +19,38 @@ def select_deck (deck_list: list[Deck])->None:
     for deck in deck_list:
         option_list.append(deck.name)
     choice, index = pick(option_list, title)
-    print(choice)
-    print(deck_list[index].cards)
+    quiz_on_deck(deck_list[index])
 
-def quiz_on_card(input: str, card: Card)->bool:
-    if not isinstance(input, str) or not isinstance (card, Card):
+def quiz_on_card(card: Card)->bool:
+    if not isinstance (card, Card):
         raise TypeError ("Not the proper value types")
     print(card.question)
-    user_answer = input
+    user_answer = input()
     if user_answer == card.answer:
         print ("Good answer !")
-        print (f"Answer: {card.answer}")
+        print (f"Answer: {card.answer}\nYour answer: {user_answer}")
         return True
     else:
         print ("Wrong answer !")
-        print (f"Answer: {card.answer}")
+        print (f"Answer: {card.answer}\nYour answer: {user_answer}")
         return False
+
+def quiz_on_deck(deck: Deck)->None:
+    print(f"Starting quiz on {deck.name}")
+    score = 0
+    card_number = 1
+    wrong_cards = []
+    for card in deck.cards:
+        print(f"{card_number}/{deck.size}")
+        result = quiz_on_card(card)
+        if result:
+            score += 1
+        else:
+            wrong_cards.append(card)
+        card_number += 1
+        print("")
+    print (f"Final score: {score}/{deck.size}")
+    print (f"Cards to review : {wrong_cards}")
+    print ("Thanks for playing !")
+    
 
--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
 #this is the program that will run.
 
 from decks import Deck, create_deck_list_from_directory
-from interface import select_language, select_deck
+from interface import select_language, select_deck, quiz_on_deck
 
 def main ():
     deck_list = create_deck_list_from_directory("./cards")
